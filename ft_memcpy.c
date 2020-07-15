54916f644db46b3e032b9fdd6354ccc9f05865a3
Barett Lavonne <49073126+blavonne@users.noreply.github.com>
2020-07-16 01:18:31 +0300

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	char		*tmp_d;
