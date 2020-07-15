a1d445c9970a8aad5c104ad2d4c45d0e9d843566
Barett Lavonne <49073126+blavonne@users.noreply.github.com>
2020-07-16 01:19:50 +0300

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*str;
